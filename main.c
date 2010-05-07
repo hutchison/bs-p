77044b5b8e5e76cb280b34ccd934f07544403333
Herbert Hutchison <hutchison@gmx.de>
2010-05-07 02:44:15 +0200

    jup jup

--- main.c
+++ main.c
@@ -11,9 +11,17 @@
  * Liste in die Ausgabedatei schreiben
  */
 
+void swapc(char *x, char *y)
+{
+    char tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
 int main(int argc, char **argv)
 {
-    FILE                *f;                                     //Eingabedatei
+    FILE                *f_in;                                     //Eingabedatei
+    FILE                *f_out;                                 //Ausgabedatei
     char                c;                                      //aktuelles Zeichen
     int                 sum = 0;                                //Gesamtanzahl der Buchstaben
     static char         *chkletters = {"modelchecking"};        //zu ueberpruefende Buchstaben
@@ -21,8 +29,9 @@ int main(int argc, char **argv)
     int                 count[laenge_chkletters];               //Anzahl eines bestimmten Buchstaben
     char                *let;                                   //Array mit allen gespeicherten Buchstaben
     struct letter_list  mylist;
+    struct letter_list  sortedlist;
     struct letter_list  *temp;
-    struct list_head    *pos;
+    //struct list_head    *pos;
 
     int                 i;
 
@@ -30,28 +39,27 @@ int main(int argc, char **argv)
 
     for (i = 0; i < laenge_chkletters; i++) count[i] = 0;
 
-    f = fopen("IO.txt","r");
+    f_in = fopen("IO.txt","r");
     let = malloc(sizeof(char));
 
-    while ( (c=fgetc(f)) != EOF ) {
+    while ( (c=fgetc(f_in)) != EOF ) {
         for (i = 0; i < laenge_chkletters; i++) {
             if (c == chkletters[i]) {
-                let[sum] = c;       //abspeichern
-                let = realloc(let, strlen(let) + sizeof(char)); //neuen Speicher fuer das naechste Element anfordern
+                let[sum] = c;
+                let = realloc(let, strlen(let) + sizeof(char));
                 count[i] += 1;
                 sum++;
             }
         }
     }
 
-    fclose(f);
+    fclose(f_in);
 
     //Anzahl der gesuchten Buchstaben ausgeben:
     for (i = 0; i < laenge_chkletters; i++) {
         printf("%c: %d\n",chkletters[i],count[i]);
     }
 
-    //Spass mit der Liste:
     INIT_LIST_HEAD(&mylist.list);
     for (i = 0; i < sum; i++) {
         temp = malloc( sizeof(struct letter_list) );
@@ -61,23 +69,40 @@ int main(int argc, char **argv)
 
     //Array mit den eingelesenen Buchstaben ausgeben:
     printf("Array der eingelesenen Buchstaben:\n");
-    printf("%s",let);
+    //printf("%s",let);
     printf("\n");
 
     //Liste mit Buchstaben ausgeben:
     printf("Liste mit Buchstaben ausgeben:\n");
-    list_for_each( pos, &mylist.list )
-    {
-        temp = list_entry( pos, struct letter_list, list );
-        printf("%c",temp->curr);
-    }
+    //list_for_each_entry( temp, &mylist.list, list ) printf("%c",temp->curr);
     printf("\n");
 
-    //Speicher freigeben:
+    i = 0;
+    INIT_LIST_HEAD(&sortedlist.list);
+    //while ( !(list_empty(&mylist.list)) ) {
+    list_for_each_entry( temp, &mylist.list, list ){
+        if( chkletters[i] == temp->curr ){
+            printf("%c",temp->curr);
+            i = (i+1) % laenge_chkletters;
+            //list_move_tail( &(temp->list), &(sortedlist.list) );
+            list_add_tail( &(temp->list), &(sortedlist.list) );
+        }
+    }
+    //list_move_tail( &(mylist.list), &(sortedlist.list) );
+    //list_for_each_entry( temp, &mylist.list, list ) list_move_tail( &(temp->list), &(sortedlist.list));
+    //list_del_init( &(mylist.list) );
+    //}
+    printf("\n");
+
+    //Liste in Datei schreiben:
+    f_out = fopen("out.txt","w");
+    list_for_each_entry( temp, &sortedlist.list, list ) fputc( temp->curr, f_out );
+    fclose(f_out);
+
     free(let);
 
-    anything[0] = '\0';
-    if(strlen(anything)) printf("anything ist noch nicht leer");
+    //anything[0] = '\0';
+    if(strlen(anything)) printf("anything ist noch nicht leer: %s\n",anything);
 
     return 0;
 }
